0f12d98a2016da9230edaafa95201d237f4c23bd
SamuelChris16 <samuelchris1112@gmail.com>
2025-11-03 14:40:46 +0700

    feat: All

--- Tugas03/script.js
+++ Tugas03/script.js
@@ -1,13 +1,14 @@
 window.addEventListener("load", () => {
   const splash = document.getElementById("splash-screen");
-  const main = document.getElementById("main-page");
+  const appPage = document.getElementById("app-page");
 
-  // tampil 5 detik penuh
+  // Splash tampil selama 5 detik
   setTimeout(() => {
-    splash.style.opacity = "0";          // fade out
+    splash.style.opacity = "0"; // mulai fade out
+
     setTimeout(() => {
-      splash.style.display = "none";     // hilangkan total
-      main.classList.add("show");        // tampilkan halaman utama
-    }, 1000); // setelah fade out selesai
-  }, 5000); // durasi splash = 5 detik
+      splash.style.display = "none"; // hilangkan splash
+      appPage.style.opacity = "1";   // tampilkan app icon
+    }, 1000); // tunggu fade-out selesai
+  }, 5000); // durasi splash: 5 detik
 });

08ca01742af30b94f5ce602ae0857229b49aeeac
nadiabaizal <nadiabaizal@gmail.com>
2025-11-07 12:07:11 +0700

    organization page + navbar interact

---
+++ Tugas01/Organization/script.js
@@ -0,0 +1,5 @@
+// JavaScript untuk halaman Organization (jika diperlukan di masa depan)
+
+document.addEventListener("DOMContentLoaded", () => {
+  // console.log("Halaman Organization dimuat!");
+});
